8e7596b9c3d4f55645d5c066e51d8cc121efac5e
WFH21 <amaanhaider270@gmail.com>
2022-06-02 23:57:05 +0530

    push

--- src/Components/Containers/ProductComponent.jsx
+++ src/Components/Containers/ProductComponent.jsx
@@ -3,34 +3,29 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const ProductComponent = () => {
-
-    const products = useSelector((state)=> state.allProducts.products);
-    const renderList = products.map((products)=>{
-
-        const {id,title,image,price,category}= products
-        return(
-            <div className="four column wide">
-                <Link to={`product/${id}`}>
-                
-             <div className="ui link cards">
-               <div className="card">  
-                <div className="image">
-                    <img src={image} alt="" /></div>
-                <div className="content">
-                 <div className="header">{title}</div>
-                 <div className="meta price ">${price}</div>
-                 <div className="meta">{category}</div>
-             </div>
-         </div>
-     </div>
-                </Link>
-    </div>
-
-
-        )
-    })
-  return <>{renderList}</>
-    
+  const products = useSelector((state) => state.allProducts.products);
+  const renderList = products.map((products) => {
+    const { id, title, image, price, category } = products;
+    return (
+        <Link to={`product/${id}`}>
+      <div className="four column wide">
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt="" />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price ">${price}</div>
+                <div className="meta">{category}</div>
+              </div>
+            </div>
+          </div>
+      </div>
+        </Link>
+    );
+  });
+  return <>{renderList}</>;
 };
 
 export default ProductComponent;
